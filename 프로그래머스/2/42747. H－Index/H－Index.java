8d630b7b344ffb56d7fd8571966a018f62d6e274
박소영 <thdudp7007@gmail.com>
2025-11-10 21:52:16 +0900

    Update H－Index.java

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -15,7 +15,9 @@ class Solution {
                 answer = cnt;
                 break;
             }
-            answer = cnt;
+            answer = cnt; // 이 부분이 없으면 [5,6,7,8] -> 4 인 테스트케이스를 통과하지 못한다.
+                          // for문이 다 끝나도 cnt >= citations[i] 인 경우를 찾지 못하기 때문
+                          // 이 경우엔 cnt 번 이상 인용된 논문의 갯수가 cnt 개 이상인 것이므로 이것이 h-index가 된다.
         }
         return answer;
     }
